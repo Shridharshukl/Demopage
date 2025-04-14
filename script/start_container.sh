a2b1a1dfaf0b488e619fea3566a5581ebfcd9be8
shridhar shukla <97231639+Shridharshukl@users.noreply.github.com>
2025-04-15 05:03:02 +0530

    add docker install cmd in start_container.sh

--- script/start_container.sh
+++ script/start_container.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-echo " docker image pull and run "
+echo "install docker and docker image pull and run "
+
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker ubuntu 
+sudo systemctl restart docker
 
 docker pull shridhar71/demo:latest
 docker run --name demoapp -p 8000:8000 -d shridhar71/demo:latest 
